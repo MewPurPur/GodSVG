f71680fe754ddcb9f19c0e8a8b6843de822883b4
ajreckof <66184050+ajreckof@users.noreply.github.com>
2023-06-19 16:06:17 +0200

    Simplify interface.gd

--- src/data_classes/SVGAttribute.gd
+++ src/data_classes/SVGAttribute.gd
@@ -6,7 +6,7 @@ var type: Type
 var value: Variant
 var default: Variant
 
-func _init(new_type: Type, new_default: Variant):
+func _init(new_type: Type, new_default: Variant, new_init : Variant = null):
 	type = new_type
 	default = new_default
-	value = new_default
+	value = new_init if new_init != null else new_default
