53008ead4e93b10ba12d5415eddb3841508c9d13
ajreckof <66184050+ajreckof@users.noreply.github.com>
2023-06-19 16:06:17 +0200

    Simplify interface.gd

--- src/data_classes/SVGTagRect.gd
+++ src/data_classes/SVGTagRect.gd
@@ -5,8 +5,8 @@ func _init():
 	attributes = {
 		"x": SVGAttribute.new(SVGAttribute.Type.FLOAT, 0.0),
 		"y": SVGAttribute.new(SVGAttribute.Type.FLOAT, 0.0),
-		"width": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0),
-		"height": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0),
+		"width": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0, 1.0),
+		"height": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0, 1.0),
 		"rx": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0),
 		"ry": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0),
 		"fill": SVGAttribute.new(SVGAttribute.Type.COLOR, "000"),
