ed9aa9ac4f0c146e149619c55bc54db7b49071b9
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-09-26 13:12:25 +0300

    Add option to ignore the limits in number fields (#34)

--- src/interface_elements/Viewport.gd
+++ src/interface_elements/Viewport.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 	SVG.data.resized.connect(resize)
 
 func resize() -> void:
-	checkerboard.size = Vector2(SVG.data.w, SVG.data.h) * 20.0
+	checkerboard.size = Vector2(SVG.data.w, SVG.data.h) * 10.0
 	checkerboard.position = (size * 1.0 - checkerboard.size) / 2
