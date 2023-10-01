5b6c4b5375c129a58ce949c977bda14aa84679da
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-10-01 16:45:15 +0300

    Add SVG import option (#39)

--- src/data_classes/SVGTagCircle.gd
+++ src/data_classes/SVGTagCircle.gd
@@ -7,7 +7,9 @@ func _init() -> void:
 		"cy": SVGAttribute.new(SVGAttribute.Type.FLOAT, 0.0),
 		"r": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0, 1.0),
 		"fill": SVGAttribute.new(SVGAttribute.Type.COLOR, "000"),
+		"fill-opacity": SVGAttribute.new(SVGAttribute.Type.NFLOAT, 1.0),
 		"stroke": SVGAttribute.new(SVGAttribute.Type.COLOR, "none"),
+		"stroke-opacity": SVGAttribute.new(SVGAttribute.Type.NFLOAT, 1.0),
 		"stroke-width": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 1.0),
 	}
 	super()
