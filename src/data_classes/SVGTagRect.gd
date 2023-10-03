15c75011fd59a2ae007d90a04918eb464ef22dee
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-10-03 18:55:10 +0300

    Add SVG path parsing (#40)

--- src/data_classes/SVGTagRect.gd
+++ src/data_classes/SVGTagRect.gd
@@ -9,7 +9,7 @@ func _init() -> void:
 		"height": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0, 1.0),
 		"rx": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0),
 		"ry": SVGAttribute.new(SVGAttribute.Type.UFLOAT, 0.0),
-		"fill": SVGAttribute.new(SVGAttribute.Type.COLOR, "000"),
+		"fill": SVGAttribute.new(SVGAttribute.Type.COLOR, "#000"),
 		"fill-opacity": SVGAttribute.new(SVGAttribute.Type.NFLOAT, 1.0),
 		"stroke": SVGAttribute.new(SVGAttribute.Type.COLOR, "none"),
 		"stroke-opacity": SVGAttribute.new(SVGAttribute.Type.NFLOAT, 1.0),
