9db995769152f85f058f5754b459733e3c28ba86
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-10-06 18:47:07 +0300

    Implement saving SVGs between sessions (#45)

--- src/inspector_elements/settings_menu.gd
+++ src/inspector_elements/settings_menu.gd
@@ -1,12 +1,17 @@
 extends ColorRect
 
 @onready var window_mode_button: CheckBox = %WindowMode
+@onready var svg_button: CheckBox = %SVG
 
 func _ready() -> void:
 	window_mode_button.button_pressed = GlobalSettings.save_window_mode
+	svg_button.button_pressed = GlobalSettings.save_svg
 
 func _on_window_mode_pressed() -> void:
 	GlobalSettings.save_window_mode = not GlobalSettings.save_window_mode
+
+func _on_svg_pressed() -> void:
+	GlobalSettings.save_svg = not GlobalSettings.save_svg
 
 func _on_close_pressed() -> void:
 	queue_free()
