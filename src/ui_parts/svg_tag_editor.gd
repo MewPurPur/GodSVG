e77f950c675267b5c820762b98f2b20f8d3e4397
MewPurPur <mew.pur.pur@gmail.com>
2023-10-17 23:42:41 +0300

    Allow to edit the viewBox

--- src/ui_parts/svg_tag_editor.gd
+++ src/ui_parts/svg_tag_editor.gd
@@ -1,13 +1,16 @@
 extends MarginContainer
 
+var coupled_viewbox := true
+
 const NumberField = preload("res://src/small_editors/number_field.tscn")
 const RectField = preload("res://src/small_editors/rect_field.tscn")
 
 signal viewbox_changed(w: float, h: float)
 
-var tag := SVG.root_tag
-
-@onready var h_box_container: HBoxContainer = $VBoxContainer/HBoxContainer
+@onready var tag := SVG.root_tag
+@onready var width_container: VBoxContainer = $Edits/Size/Width
+@onready var height_container: VBoxContainer = $Edits/Size/Height
+@onready var viewbox_container: VBoxContainer = $Edits/ViewBox
 
 var width_edit: AttributeEditor
 var height_edit: AttributeEditor
@@ -18,29 +21,42 @@ func _ready() -> void:
 	width_edit.allow_lower = false
 	width_edit.attribute = tag.attributes.width
 	width_edit.attribute_name = "width"
-	h_box_container.add_child(width_edit)
-	width_edit.value_changed.connect(sync_dimensions_and_viewbox.unbind(1))
+	width_container.add_child(width_edit)
+	width_edit.value_changed.connect(update_svg_attributes.unbind(1))
 	
 	height_edit = NumberField.instantiate()
 	height_edit.allow_lower = false
 	height_edit.attribute = tag.attributes.height
 	height_edit.attribute_name = "height"
-	h_box_container.add_child(height_edit)
-	height_edit.value_changed.connect(sync_dimensions_and_viewbox.unbind(1))
+	height_container.add_child(height_edit)
+	height_edit.value_changed.connect(update_svg_attributes.unbind(1))
 	
 	viewbox_edit = RectField.instantiate()
 	viewbox_edit.attribute = tag.attributes.viewBox
 	viewbox_edit.attribute_name = "viewBox"
-	h_box_container.add_child(viewbox_edit)
+	viewbox_container.add_child(viewbox_edit)
+	viewbox_edit.value_changed.connect(update_svg_attributes.unbind(1))
 	
-	sync_dimensions_and_viewbox()
-	update_viewbox()
+	determine_viewbox_edit()
+	update_svg_attributes()
 
 
-func update_viewbox() -> void:
-	width_edit.set_value(SVG.root_tag.attributes.width.value, false)
-	height_edit.set_value(SVG.root_tag.attributes.height.value, false)
-	viewbox_edit.set_value(SVG.root_tag.attributes.viewBox.value)
+func update_svg_attributes() -> void:
+	var new_width_value: float= SVG.root_tag.attributes.width.value
+	var new_height_value: float = SVG.root_tag.attributes.height.value
+	width_edit.set_value(new_width_value, false)
+	height_edit.set_value(new_height_value, false)
+	if coupled_viewbox:
+		viewbox_edit.set_value(Rect2(0, 0, new_width_value, new_height_value))
+	else:
+		viewbox_edit.set_value(SVG.root_tag.attributes.viewBox.value)
 
-func sync_dimensions_and_viewbox() -> void:
-	viewbox_edit.set_value(Rect2(0, 0, width_edit.get_value(), height_edit.get_value()))
+
+func _on_couple_button_toggled(toggled_on: bool) -> void:
+	coupled_viewbox = toggled_on
+	determine_viewbox_edit()
+
+func determine_viewbox_edit() -> void:
+	for number_edit in viewbox_edit.get_children():
+		number_edit.num_edit.editable = not coupled_viewbox
+	update_svg_attributes()
