5972fba287f985d73a7b26e558f7ed835b6a9f51
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-10-19 18:11:01 +0300

    Make path command editors more minimal (#93)

--- src/small_editors/mini_number_field.gd
+++ src/small_editors/mini_number_field.gd
@@ -1,13 +1,7 @@
-extends AttributeEditor
+extends BetterLineEdit
 
-@onready var num_edit: LineEdit = $LineEdit
-
-var min_value := 0.0
-var max_value := 1.0
-var allow_lower := true
-var allow_higher := true
-
-var is_float := true
+enum Mode {DEFAULT, ONLY_POSITIVE, ANGLE}
+var mode := Mode.DEFAULT
 
 signal value_changed(new_value: float)
 var _value: float  # Must not be updated directly.
@@ -17,46 +11,35 @@ func set_value(new_value: float, emit_value_changed := true):
 	_value = validate(new_value)
 	if _value != old_value and emit_value_changed:
 		value_changed.emit(_value)
-	elif num_edit != null:
-		num_edit.text = String.num(_value, 4)
-		set_text_tint()
+	
+	text = String.num(_value, 4)
 
 func get_value() -> float:
 	return _value
 
 
 func _ready() -> void:
+	super()
 	value_changed.connect(_on_value_changed)
-	if attribute != null:
-		set_value(attribute.value)
-		attribute.value_changed.connect(set_value)
-		set_text_tint()
-		num_edit.tooltip_text = attribute_name
-	num_edit.text = str(get_value())
+	text = str(get_value())
 
 func validate(new_value: float) -> float:
-	if allow_lower:
-		if allow_higher:
-			return new_value
-		else:
-			return minf(new_value, max_value)
-	else:
-		if allow_higher:
-			return maxf(new_value, min_value)
-		else:
-			return clampf(new_value, min_value, max_value)
+	match mode:
+		Mode.ONLY_POSITIVE: return maxf(new_value, 0.0001)
+		Mode.ANGLE: return clampf(-360.0, new_value, 360.0)
+		_: return new_value
 
 func _on_value_changed(new_value: float) -> void:
-	num_edit.text = String.num(new_value, 4)
-	if attribute != null:
-		attribute.value = new_value
+	text = String.num(new_value, 4)
 
 
 func _on_focus_exited() -> void:
-	set_value(calculate_expression(num_edit.text))
+	set_value(calculate_expression(text))
+	super()
 
 func _on_text_submitted(submitted_text: String) -> void:
 	set_value(calculate_expression(submitted_text))
+	super(submitted_text)
 
 # This function evaluates expressions even if "," or ";" is used as a decimal separator.
 func calculate_expression(text: String) -> float:
@@ -81,16 +64,3 @@ func calculate_expression(text: String) -> float:
 			return result
 	
 	return get_value()
-
-
-func add_tooltip(text: String) -> void:
-	if num_edit == null:
-		await ready
-	num_edit.tooltip_text = text
-
-func set_text_tint() -> void:
-	if num_edit != null:
-		if attribute != null and get_value() == attribute.default:
-			num_edit.add_theme_color_override(&"font_color", Color(0.64, 0.64, 0.64))
-		else:
-			num_edit.remove_theme_color_override(&"font_color")
