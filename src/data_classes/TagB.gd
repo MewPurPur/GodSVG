4e4716a38fcc039d9e8f44a3f1d2c4485a1654c5
Serem Titus(serem_empire) <seremempire@gmail.com>
2023-10-22 21:55:27 +0300

    simplify undo_redo actions

--- src/data_classes/TagB.gd
+++ src/data_classes/TagB.gd
@@ -10,12 +10,11 @@ var child_tags: Array[Tag]
 
 func add_tag(new_tag: Tag,create_undo_redo:bool = true) -> void:
 	if create_undo_redo:
-		EditorUndoRedo.undo_redo.create_action("Add tag")
-		EditorUndoRedo.undo_redo.add_do_reference(new_tag)
-		EditorUndoRedo.undo_redo.add_undo_reference(new_tag)
-		EditorUndoRedo.undo_redo.add_do_method(add_tag.bind(new_tag,false))
-		EditorUndoRedo.undo_redo.add_undo_method(delete_tag_with_reference.bind(new_tag,false))
-		EditorUndoRedo.undo_redo.commit_action()
+		tagb_undo_redo_action(
+			"Add tag",
+			add_tag.bind(new_tag,false),
+			delete_tag_with_reference.bind(new_tag,false),
+			new_tag)
 		return
 	child_tags.append(new_tag)
 	new_tag.attribute_changed.connect(emit_child_tag_attribute_changed)
@@ -35,12 +34,11 @@ func replace_tags(new_tags: Array[Tag]) -> void:
 func delete_tag(idx: int,create_undo_redo:bool = true) -> void:
 	if create_undo_redo:
 		var tag:Tag = child_tags[idx]
-		EditorUndoRedo.undo_redo.create_action("Delete tag")
-		EditorUndoRedo.undo_redo.add_do_reference(tag)
-		EditorUndoRedo.undo_redo.add_undo_reference(tag)
-		EditorUndoRedo.undo_redo.add_do_method(delete_tag_with_reference.bind(tag,false))
-		EditorUndoRedo.undo_redo.add_undo_method(add_tag_and_move_to.bind(tag,idx))
-		EditorUndoRedo.undo_redo.commit_action()
+		tagb_undo_redo_action(
+			"Delete tag",
+			delete_tag_with_reference.bind(tag,false),
+			add_tag_and_move_to.bind(tag,idx),
+			tag)
 		return
 	if idx >= 0:
 		child_tags.remove_at(idx)
@@ -52,10 +50,10 @@ func delete_tag_with_reference(tag:Tag,create_undo_redo:bool = true) -> void:
 
 func move_tag(old_idx: int, new_idx: int,create_undo_redo:bool = true) -> void:
 	if create_undo_redo:
-		EditorUndoRedo.undo_redo.create_action("Move tag")
-		EditorUndoRedo.undo_redo.add_do_method(move_tag.bind(old_idx , new_idx,false))
-		EditorUndoRedo.undo_redo.add_undo_method(move_tag.bind(new_idx, old_idx,false))
-		EditorUndoRedo.undo_redo.commit_action()
+		tagb_undo_redo_action(
+			"Move tag",
+			move_tag.bind(old_idx , new_idx,false),
+			move_tag.bind(new_idx, old_idx,false))
 		return
 	var tag: Tag = child_tags.pop_at(old_idx)  # Should be inferrable, GDScript bug.
 	child_tags.insert(new_idx, tag)
@@ -65,12 +63,11 @@ func duplicate_tag(idx: int, create_undo_redo:bool = true) -> void:
 	var new_tag :Tag = child_tags[idx].duplicate()
 	new_tag.attribute_changed.connect(emit_child_tag_attribute_changed)
 	if create_undo_redo:
-		EditorUndoRedo.undo_redo.create_action("Duplicate tag")
-		EditorUndoRedo.undo_redo.add_do_reference(new_tag)
-		EditorUndoRedo.undo_redo.add_undo_reference(new_tag)
-		EditorUndoRedo.undo_redo.add_do_method(add_tag_and_move_to.bind(new_tag,idx + 1))
-		EditorUndoRedo.undo_redo.add_undo_method(delete_tag_with_reference.bind(new_tag,false))
-		EditorUndoRedo.undo_redo.commit_action()
+		tagb_undo_redo_action(
+			"Duplicate tag",
+			add_tag_and_move_to.bind(new_tag,idx + 1),
+			delete_tag_with_reference.bind(new_tag,false),
+			new_tag)
 		return
 	add_tag_and_move_to(new_tag,idx + 1)
 	
@@ -79,3 +76,11 @@ func emit_child_tag_attribute_changed() -> void:
 
 func get_child_count() -> int:
 	return child_tags.size()
+
+func tagb_undo_redo_action(name:String,do:Callable,undo:Callable,add_reference:Object = null) -> void:
+	EditorUndoRedo.undo_redo.create_action(name)
+	EditorUndoRedo.undo_redo.add_do_reference(add_reference)
+	EditorUndoRedo.undo_redo.add_undo_reference(add_reference)
+	EditorUndoRedo.undo_redo.add_do_method(do)
+	EditorUndoRedo.undo_redo.add_undo_method(undo)
+	EditorUndoRedo.undo_redo.commit_action()
