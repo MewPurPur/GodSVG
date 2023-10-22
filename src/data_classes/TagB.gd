29792481faa522688ad6a62f21d30f2c9f938ac6
Serem Titus(serem_empire) <seremempire@gmail.com>
2023-10-22 20:22:35 +0300

    add undoredo to delete_tag

--- src/data_classes/TagB.gd
+++ src/data_classes/TagB.gd
@@ -14,12 +14,17 @@ func add_tag(new_tag: Tag,create_undo_redo:bool = true) -> void:
 		EditorUndoRedo.undo_redo.add_do_reference(new_tag)
 		EditorUndoRedo.undo_redo.add_undo_reference(new_tag)
 		EditorUndoRedo.undo_redo.add_do_method(add_tag.bind(new_tag,false))
-		EditorUndoRedo.undo_redo.add_undo_method(delete_tag_with_reference.bind(new_tag))
+		EditorUndoRedo.undo_redo.add_undo_method(delete_tag_with_reference.bind(new_tag,false))
 		EditorUndoRedo.undo_redo.commit_action()
 		return
 	child_tags.append(new_tag)
 	new_tag.attribute_changed.connect(emit_child_tag_attribute_changed)
 	tag_added.emit()
+
+func add_tag_and_move_to(new_tag:Tag,to_idx:int):
+	add_tag(new_tag)
+	var from_idx = child_tags.find(new_tag)
+	move_tag(from_idx,to_idx)
 
 func replace_tags(new_tags: Array[Tag]) -> void:
 	child_tags.clear()
@@ -28,14 +33,23 @@ func replace_tags(new_tags: Array[Tag]) -> void:
 		tag.attribute_changed.connect(emit_child_tag_attribute_changed)
 	changed_unknown.emit()
 
-func delete_tag(idx: int) -> void:
+func delete_tag(idx: int,create_undo_redo:bool = true) -> void:
+	if create_undo_redo:
+		var tag:Tag = child_tags[idx]
+		EditorUndoRedo.undo_redo.create_action("delete tag")
+		EditorUndoRedo.undo_redo.add_do_reference(tag)
+		EditorUndoRedo.undo_redo.add_undo_reference(tag)
+		EditorUndoRedo.undo_redo.add_do_method(delete_tag_with_reference.bind(tag,false))
+		EditorUndoRedo.undo_redo.add_undo_method(add_tag_and_move_to.bind(tag,idx))
+		EditorUndoRedo.undo_redo.commit_action()
+		return
 	if idx >= 0:
 		child_tags.remove_at(idx)
 		tag_deleted.emit(idx)
 
-func delete_tag_with_reference(tag:Tag) -> void:
+func delete_tag_with_reference(tag:Tag,create_undo_redo:bool = true) -> void:
 	var idx = child_tags.find(tag)
-	delete_tag(idx)
+	delete_tag(idx,create_undo_redo)
 
 func move_tag(old_idx: int, new_idx: int) -> void:
 	var tag: Tag = child_tags.pop_at(old_idx)  # Should be inferrable, GDScript bug.
