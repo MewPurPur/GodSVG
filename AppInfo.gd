da2dfea9a5dc6072058ca0c8540abdb0d17a6f4b
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-10-26 14:36:14 +0300

    Add AppInfo object for fetching app information (#127)

---
+++ AppInfo.gd
@@ -0,0 +1,14 @@
+class_name AppInfo extends RefCounted
+
+const version := "1.0.dev"
+
+const project_founders: Array[String] = ["MewPurPur"]
+const project_managers: Array[String] = ["MewPurPur"]
+
+const authors: Array[String] = [
+	"ajreckof",
+	"Kiisu-Master",
+	"MewPurPur",
+	"Serem Titus (SeremTitus)",
+	"Swarkin"
+]
