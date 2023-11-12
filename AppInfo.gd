e779fde241684a607e86ad65ebfab32d85e1ad95
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-11-12 17:07:09 +0200

    Clarify authorship

--- AppInfo.gd
+++ AppInfo.gd
@@ -6,6 +6,11 @@ const version := "1.0.dev"
 const project_founders: Array[String] = ["MewPurPur"]
 const project_managers: Array[String] = ["MewPurPur"]
 
+
+# The developers who contributed significant patches to the MIT-licensed source code
+# of GodSVG are listed here. What counts as "significant" is decided arbitrarily.
+# The format is either "Real Name (GitHub username)" or "GitHub username".
+
 const authors: Array[String] = [
 	"ajreckof",
 	"Kiisu-Master",
