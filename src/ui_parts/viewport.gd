ca0d15830b72b48870432db52957b4ce6760c65b
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-11-15 12:19:29 +0200

    Improve inspector performance by preloading context popups (#213)

--- src/ui_parts/viewport.gd
+++ src/ui_parts/viewport.gd
@@ -5,7 +5,7 @@ const buffer_view_space = 0.8
 @onready var display: TextureRect = %Checkerboard
 @onready var view: Camera2D = $ViewCamera
 @onready var controls: Control = %Checkerboard/Controls
-@onready var display_texture: TextureRect = %Checkerboard/DisplayTexture
+@onready var display_texture: Control = %Checkerboard/DisplayTexture
 @onready var zoom_menu: HBoxContainer = %ZoomMenu
 @onready var main_node: VBoxContainer = %Display
 
