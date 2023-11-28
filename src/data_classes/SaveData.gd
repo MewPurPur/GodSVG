c58026e1fbc6b2c6a74a78ec53dadffdadd2eee6
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2023-11-28 23:03:00 +0200

    Remember last used directory for importing and exporting.

--- src/data_classes/SaveData.gd
+++ src/data_classes/SaveData.gd
@@ -9,3 +9,4 @@ const GoodColorPicker = preload("res://src/ui_elements/good_color_picker.gd")
 @export var snap := -0.5  # Negative when disabled.
 @export var color_picker_slider_mode := GoodColorPicker.SliderMode.RGB
 @export var path_command_relative := false
+@export var last_used_dir := ""
