d95a2d5bbdaa2a60e188c6147579a45e53a706fa
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2023-11-29 22:02:54 +0200

    Fix context menu popup logic (#267)

--- src/Utils.gd
+++ src/Utils.gd
@@ -192,7 +192,7 @@ static func popup_under_control_centered(popup: Popup, control: Control) -> void
 	popup.popup(Rect2(popup_pos, popup.size))
 
 static func popup_under_mouse(popup: Popup, mouse_pos: Vector2) -> void:
-	popup.popup(Rect2(mouse_pos + Vector2(-popup.size.x / 2, 0), popup.size))
+	popup.popup(Rect2(mouse_pos, popup.size))
 
 
 static func get_cubic_bezier_points(cp1: Vector2, cp2: Vector2,
