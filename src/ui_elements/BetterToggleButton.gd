8d2785e3ac2deb0913af9dd1c4a79044f16033d3
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2023-11-30 21:57:06 +0200

    Fix root tag editor text buttons not having hover indication (#272)

--- src/ui_elements/BetterToggleButton.gd
+++ src/ui_elements/BetterToggleButton.gd
@@ -6,16 +6,21 @@ var hovered := false
 ## Overlayed on top when the Button is hovered while pressed.
 @export var hover_pressed_stylebox: StyleBox
 
+@export var font_hover_pressed_color: Color
+
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 
 func _on_mouse_entered() -> void:
 	hovered = true
+	if not font_hover_pressed_color == Color.BLACK and button_pressed:
+		add_theme_color_override("font_pressed_color", font_hover_pressed_color)
 	queue_redraw()
 
 func _on_mouse_exited() -> void:
 	hovered = false
+	remove_theme_color_override("font_pressed_color")
 	queue_redraw()
 
 func _draw() -> void:
