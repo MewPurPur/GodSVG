45233efabd204b8cf9fad1c03a2c75696f5c7bb1
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2023-12-01 16:16:10 +0200

    relative camera movement

--- src/ui_parts/viewport_container.gd
+++ src/ui_parts/viewport_container.gd
@@ -1,12 +1,11 @@
 extends SubViewportContainer
 
 var last_pos: Vector2
-var wraps: Vector2
+var wrapped: bool
 
 func wrap_mouse(already_moved: bool) -> Vector2:
 	if not already_moved:
-		wraps = Vector2(0,0)
-		last_pos = Vector2(0,0)
+		last_pos = DisplayServer.mouse_get_position()
 	var view_rect := get_global_rect()
 	view_rect.position += Vector2(get_window().get_position())
 	print(view_rect)
@@ -15,21 +14,22 @@ func wrap_mouse(already_moved: bool) -> Vector2:
 		if mouse_pos.x < view_rect.position.x:
 			print("too left")
 			mouse_pos.x = view_rect.position.x + view_rect.size.x
-			wraps.x -= 1
 		if mouse_pos.x > (view_rect.position.x + view_rect.size.x):
 			print("too right")
 			mouse_pos.x = view_rect.position.x
-			wraps.x += 1
 		if mouse_pos.y < view_rect.position.y:
 			print("too up")
 			mouse_pos.y = view_rect.position.y + view_rect.size.y
-			wraps.y -= 1
 		if mouse_pos.y >  view_rect.position.y + view_rect.size.y:
 			print("too down")
 			mouse_pos.y = view_rect.position.y
-			wraps.y += 1
+		wrapped = true
 	var win_pos: Vector2 = get_window().get_position() # probably the global cords window position
 	DisplayServer.warp_mouse(mouse_pos - win_pos) # adds window position by default
-	var relative = mouse_pos - last_pos + view_rect.size * wraps
+	var relative: Vector2 = mouse_pos - last_pos
 	last_pos = DisplayServer.mouse_get_position()
-	return relative
+	if not wrapped:
+		return relative
+	else:
+		wrapped = false
+		return Vector2(0,0)
