79fb4b569c4afc5ba849f414411bb3b6e9ce0e9b
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2023-12-01 17:05:34 +0200

    use fposmod

--- src/ui_parts/viewport_container.gd
+++ src/ui_parts/viewport_container.gd
@@ -10,18 +10,10 @@ func wrap_mouse(already_moved: bool) -> Vector2:
 	var view_rect := get_global_rect()
 	var mouse_pos: Vector2 = DisplayServer.mouse_get_position()
 	view_rect.position += Vector2(get_window().get_position())
-	
 	if not view_rect.has_point(mouse_pos):
-		if mouse_pos.x < view_rect.position.x:
-			mouse_pos.x = view_rect.position.x + view_rect.size.x
-		if mouse_pos.x > (view_rect.position.x + view_rect.size.x):
-			mouse_pos.x = view_rect.position.x
-		if mouse_pos.y < view_rect.position.y:
-			mouse_pos.y = view_rect.position.y + view_rect.size.y
-		if mouse_pos.y >  view_rect.position.y + view_rect.size.y:
-			mouse_pos.y = view_rect.position.y
+		mouse_pos.x = fposmod(mouse_pos.x - view_rect.position.x, view_rect.size.x) + view_rect.position.x
+		mouse_pos.y = fposmod(mouse_pos.y - view_rect.position.y, view_rect.size.y) + view_rect.position.y
 		wrapped = true
-		
 	var win_pos: Vector2 = get_window().get_position()
 	DisplayServer.warp_mouse(mouse_pos - win_pos)
 	
