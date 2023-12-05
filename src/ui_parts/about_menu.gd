a29e8a3c00a7ce661ab6f033361db00dbc32690b
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2023-12-05 13:41:05 +0200

    Setup exports (#286)

--- src/ui_parts/about_menu.gd
+++ src/ui_parts/about_menu.gd
@@ -4,7 +4,8 @@ extends Dialog
 @onready var version_label: Label = %VersionLabel
 
 func _ready() -> void:
-	version_label.text = "GodSVG " + AppInfo.version
+	version_label.text = "GodSVG " + ProjectSettings.get_setting(
+			&"application/config/version", "Version information unavailable")
 	add_section("Project Founder", AppInfo.project_founders)
 	add_section("Project Manager", AppInfo.project_managers)
 	add_section("Developers", AppInfo.authors)
