d0f9968e195ff01d4e428af3eec7c4310163264a
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2023-12-06 18:15:58 +0200

    Less empty lines

--- src/ui_parts/viewport.gd
+++ src/ui_parts/viewport.gd
@@ -104,9 +104,7 @@ func _on_size_changed() -> void:
 func wrap_mouse(relative: Vector2) -> Vector2:
 	var view_rect := get_visible_rect().grow(-1.0)
 	var mouse_pos := get_mouse_position()
-	
 	var warp_margin := view_rect.size * 0.5
-	
 	var relative_sign := relative.sign()
 	
 	var relative_warped := Vector2(
