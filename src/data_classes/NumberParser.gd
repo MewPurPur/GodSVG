d8bf9009a0108472bfe0bba3f5eebb8e8da0467c
MewPurPur <mew.pur.pur@gmail.com>
2023-12-30 16:43:32 +0200

    Fix Center Frame not redrawing the grid

--- src/data_classes/NumberParser.gd
+++ src/data_classes/NumberParser.gd
@@ -2,12 +2,11 @@ class_name NumberParser extends RefCounted
 
 static func num_to_text(number: float) -> String:
 	var output := String.num(number, 4)
-	if GlobalSettings.number_remove_leading_zero and output.find(".") != -1:
+	if GlobalSettings.number_remove_leading_zero and "." in output:
 		if output.begins_with("0"):
 			output = output.right(-1)
 		elif output.begins_with("-0") or output.begins_with("+0"):
 			output = output.erase(1)
-	
 	return output
 
 static func text_to_num(text: String) -> float:
@@ -15,40 +14,26 @@ static func text_to_num(text: String) -> float:
 
 # The passed text should already be a valid number.
 static func format_text(text: String) -> String:
-	if GlobalSettings.number_remove_plus_sign:
-		if text.begins_with("+"):
+	var unary_plus := GlobalSettings.number_remove_plus_sign and text.begins_with("+")
+	var leading_decimal_point := text.begins_with(".") or text.begins_with("+.") or\
+			text.begins_with("-.")
+	var padded_zeros := 0
+	while text.ends_with("0"):
+		text = text.left(-1)
+		padded_zeros += 1
+	
+	text = String.num(text.to_float(), 4)
+	
+	if leading_decimal_point or\
+	(GlobalSettings.number_remove_leading_zero and "." in text):
+		if text.begins_with("0"):
 			text = text.right(-1)
+		if text.begins_with("-0") or text.begins_with("+0"):
+			text = text.erase(1)
 	
-	var decimal_point_pos := text.find(".")
-	if decimal_point_pos != -1 and text.length() > decimal_point_pos + 5:
-		var round_up := text[decimal_point_pos + 5] in ["5", "6", "7", "8", "9"]
-		text = text.left(decimal_point_pos + 5)
-		if round_up:
-			while true:
-				var last_char := text[text.length() - 1]
-				if last_char == "9":
-					text = text.left(-1)
-				elif last_char == ".":
-					text = text.left(-1)
-					text[text.length() - 1] = String.num_uint64(
-							text[text.length() - 1].to_int() + 1)
-					break
-				else:
-					text[text.length() - 1] = String.num_uint64(last_char.to_int() + 1)
-					break
-	
-	if GlobalSettings.number_remove_leading_zero:
-		decimal_point_pos = text.find(".")
-		if decimal_point_pos != -1:
-			while text.begins_with("0"):
-				text = text.right(-1)
-			while text.begins_with("-0") or text.begins_with("+0"):
-				text = text.erase(1)
-	
-	if GlobalSettings.number_remove_zero_padding:
-		decimal_point_pos = text.find(".")
-		if decimal_point_pos != -1:
-			while text[text.length() - 1] in ["0", "."]:
-				text = text.left(-1)
+	text += "0".repeat(padded_zeros)
+	text = text.left(text.find(".") + 5)
+	if unary_plus:
+		text = "+" + text
 	
 	return text
