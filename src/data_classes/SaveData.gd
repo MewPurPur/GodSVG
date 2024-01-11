fd75b7398f3ad07fca1f566b98c399210833bceb
MewPurPur <mew.pur.pur@gmail.com>
2024-01-11 17:38:05 +0200

    Fix up various things

--- src/data_classes/SaveData.gd
+++ src/data_classes/SaveData.gd
@@ -10,3 +10,10 @@ const GoodColorPicker = preload("res://src/ui_elements/good_color_picker.gd")
 @export var color_picker_slider_mode := GoodColorPicker.SliderMode.RGB
 @export var path_command_relative := false
 @export var last_used_dir := ""
+
+signal current_file_path_changed
+@export var current_file_path := "":
+	set(new_value):
+		if new_value != current_file_path:
+			current_file_path = new_value
+			current_file_path_changed.emit()
