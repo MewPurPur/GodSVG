7c73f334d7616318566ded41cec60e24dcf4fd53
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-01-22 14:49:12 +0200

    Improve transform editor a little (#422)

--- src/data_classes/TagCircle.gd
+++ src/data_classes/TagCircle.gd
@@ -10,7 +10,7 @@ const known_inheritable_attributes = ["transform", "opacity", "fill", "fill-opac
 
 func _init() -> void:
 	attributes = {
-		"transform": AttributeTransform.new("matrix(1 0 0 1 0 0)"),
+		"transform": AttributeTransform.new(),
 		"cx": AttributeNumeric.new(AttributeNumeric.Mode.FLOAT, "0"),
 		"cy": AttributeNumeric.new(AttributeNumeric.Mode.FLOAT, "0"),
 		"r": AttributeNumeric.new(AttributeNumeric.Mode.UFLOAT, "0", "1"),
