5397210ff3955f7b5be572d4da8290c798e27866
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-01-26 13:24:22 +0200

    Fix elliptical curve radii widget (#436)

--- src/ui_elements/mini_number_field.gd
+++ src/ui_elements/mini_number_field.gd
@@ -23,16 +23,22 @@ func get_value() -> float:
 
 
 func validate(new_value: float) -> float:
+	#
 	match mode:
-		Mode.ONLY_POSITIVE: return maxf(new_value, 0.0001)
 		Mode.HALF_ANGLE: return fmod(new_value, 180.0)
 		Mode.ANGLE: return fmod(new_value, 360.0)
 		_: return new_value
 
 
 func _on_focus_exited() -> void:
-	set_value(AttributeNumeric.evaluate_expr(text))
+	set_value(evaluate_after_input(text))
 	super()
 
 func _on_text_submitted(submitted_text: String) -> void:
-	set_value(AttributeNumeric.evaluate_expr(submitted_text))
+	set_value(evaluate_after_input(submitted_text))
+
+func evaluate_after_input(eval_text: String) -> float:
+	var num := AttributeNumeric.evaluate_expr(eval_text)
+	if mode == Mode.ONLY_POSITIVE:
+		num = maxf(num, 0.0001)
+	return num
