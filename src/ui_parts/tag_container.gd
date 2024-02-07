a3738abd9a3387c1ab7f65379e7248ea345eb23b
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-02-07 12:09:01 +0200

    Draw the path command editor widgets until it's hovered (#471)

--- src/ui_parts/tag_container.gd
+++ src/ui_parts/tag_container.gd
@@ -60,7 +60,6 @@ func update_proposed_tid() -> void:
 		Indications.set_proposed_drop_tid(Utils.get_parent_tid(next_tid) +\
 				PackedInt32Array([next_tid[-1] + 1]))
 	elif Utils.is_tid_parent_or_self(prev_tid, next_tid):
-		var zeros_at_tid_end := 0
 		for i in range(prev_tid.size(), next_tid.size()):
 			if next_tid[i] != 0:
 				return
