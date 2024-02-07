16cbb6c8055f890d6d17cb4a9acd4ea7faf5885a
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2024-02-07 22:52:45 +0200

    Oops

--- src/ui_elements/BetterTextEdit.gd
+++ src/ui_elements/BetterTextEdit.gd
@@ -16,8 +16,8 @@ var hovered := false
 func _init() -> void:
 	context_menu_enabled = false
 	wrap_mode = TextEdit.LINE_WRAPPING_BOUNDARY
-	#scroll_smooth = true
-	#scroll_v_scroll_speed = 30.0
+	scroll_smooth = true
+	scroll_v_scroll_speed = 30.0
 	caret_multiple = false
 	highlight_all_occurrences = true
 
