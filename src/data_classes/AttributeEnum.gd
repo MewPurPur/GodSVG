17ba5c9c5d8f3bd779b1931ffdc12b8f1d46b32a
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-02-14 22:33:25 +0200

    Force enums to have correct values (#498)

--- src/data_classes/AttributeEnum.gd
+++ src/data_classes/AttributeEnum.gd
@@ -7,3 +7,6 @@ func _init(new_possible_values: Array[String], new_default_idx := 0) -> void:
 	possible_values = new_possible_values
 	default = possible_values[new_default_idx]
 	set_value(default, SyncMode.SILENT)
+
+func set_value(new_value: String, sync_mode := SyncMode.LOUD) -> void:
+	super(new_value if new_value in possible_values else default, sync_mode)
