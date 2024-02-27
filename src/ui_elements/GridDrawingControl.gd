409d7eac50e9e53580c1e0f74fcb1a64c1fff0cb
MewPurPur <mew.pur.pur@gmail.com>
2024-02-27 14:55:02 +0200

    Improve looks of About dialog

---
+++ src/ui_elements/GridDrawingControl.gd
@@ -0,0 +1,16 @@
+class_name PanelGrid extends GridContainer
+
+const font = preload("res://visual/fonts/Font.ttf")
+
+@export var items: Array[String]
+@export var stylebox: StyleBox
+
+func setup() -> void:
+	for item in items:
+		var panel_container := PanelContainer.new()
+		panel_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		panel_container.add_theme_stylebox_override(&"panel", stylebox)
+		var label := Label.new()
+		label.text = item
+		panel_container.add_child(label)
+		add_child(panel_container)
