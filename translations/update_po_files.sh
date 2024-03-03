1de4c90f19e6c50cf3309d40d8644d273e19c260
Kiisu_Master <142301391+Kiisu-Master@users.noreply.github.com>
2024-03-03 15:51:42 +0200

    Gettext followup (#566)

--- translations/update_po_files.sh
+++ translations/update_po_files.sh
@@ -3,7 +3,7 @@ printf "\nFound $count po files. Starting update..."
 for file in ./*.po; do
     printf "\n\n$count files left.\nUpdating $file: "
     let count--
-    msgmerge --update --quiet --verbose --backup=off $file Translations.pot
+    msgmerge --update --quiet --verbose --backup=off $file GodSVG.pot
     if [ "$?" != "0" ]; then # Exit loop on error.
         printf "\nUpdating failed, exiting.\n"
         exit
