7b8323a5e7a78e52d186649d625ef4c64d9a98aa
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-03-07 23:47:37 +0200

    Refactor savefiles and implement configurable shortcuts (#570)

--- src/ui_parts/export_dialog.gd
+++ src/ui_parts/export_dialog.gd
@@ -52,7 +52,7 @@ func _on_ok_button_pressed() -> void:
 	if OS.has_feature("web"):
 		match extension:
 			"png":
-				HandlerGUI.web_save_png(_create_img())
+				HandlerGUI.web_save_png(create_image())
 			_:
 				HandlerGUI.web_save_svg()
 	else:
@@ -66,7 +66,7 @@ func export(path: String) -> void:
 	
 	match extension:
 		"png":
-			_create_img().save_png(path)
+			create_image().save_png(path)
 		_:
 			# SVG / fallback.
 			GlobalSettings.modify_save_data("current_file_path", path)
@@ -89,8 +89,10 @@ func update_final_scale() -> void:
 func update_extension_configuration() -> void:
 	scale_container.visible = (extension == "png")
 
-func _create_img() -> Image:
+func create_image() -> Image:
 	var export_svg := SVG.root_tag.create_duplicate()
+	if export_svg.attributes.viewBox.get_list().is_empty():
+		export_svg.attributes.viewBox.set_list([0, 0, export_svg.width, export_svg.height])
 	export_svg.attributes.width.set_num(export_svg.width * upscale_amount)
 	export_svg.attributes.height.set_num(export_svg.height * upscale_amount)
 	var img := Image.new()
