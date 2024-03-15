534a2384cebe4f24dca72e146e84bf0e54a981cc
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-03-15 03:19:08 +0200

    Implement an optional alpha for the color picker (#588)

--- src/ui_elements/color_picker_popup.gd
+++ src/ui_elements/color_picker_popup.gd
@@ -1,17 +1,20 @@
 extends Popup
 
-const GoodColorPickerType = preload("res://src/ui_elements/good_color_picker.gd")
+const GoodColorPicker = preload("res://src/ui_elements/good_color_picker.tscn")
 
-@onready var picker: GoodColorPickerType = $PanelContainer/MarginContainer/ColorPicker
+@onready var margin_container: MarginContainer = $PanelContainer/MarginContainer
 
 signal color_picked(new_color: String, final: bool)
 var enable_alpha := false
 var current_value: String
 
 func _ready() -> void:
+	var color_picker := GoodColorPicker.instantiate()
+	color_picker.alpha_enabled = enable_alpha
+	margin_container.add_child(color_picker)
 	await get_tree().process_frame
-	picker.setup_color(current_value)
-	picker.enable_alpha = enable_alpha
+	color_picker.setup_color(current_value)
+	color_picker.color_changed.connect(pick_color)
 
 func pick_color(color: String) -> void:
 	color_picked.emit(color, false)
