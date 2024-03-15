1b5bdd909fd8f668d984912e5b1c3bb34bd05e55
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-03-15 03:19:08 +0200

    Implement an optional alpha for the color picker (#588)

--- src/ui_elements/setting_color.gd
+++ src/ui_elements/setting_color.gd
@@ -4,13 +4,15 @@ signal value_changed
 
 @export var section_name: String
 @export var setting_name: String
+@export var enable_alpha := true
 
 @onready var color_edit: HBoxContainer = $PanelContainer/HBoxContainer/ColorEdit
 @onready var label: Label = $Label
 @onready var reset_button: Button = $PanelContainer/HBoxContainer/ResetButton
 
 func _ready() -> void:
-	color_edit.value = GlobalSettings.get(setting_name).to_html(false)
+	color_edit.enable_alpha = true
+	color_edit.value = GlobalSettings.get(setting_name).to_html(enable_alpha)
 	reset_button.tooltip_text = tr("Reset to default")
 
 func _on_color_edit_value_changed(new_value: String) -> void:
@@ -24,7 +26,9 @@ func _on_reset_button_pressed() -> void:
 	update_widgets()
 
 func update_widgets() -> void:
-	var setting_value: String = GlobalSettings.get(setting_name).to_html(false)
-	color_edit.value = setting_value
-	reset_button.visible = (setting_value !=\
-			GlobalSettings.default_config[section_name][setting_name].to_html(false))
+	var setting_value: Color = GlobalSettings.get(setting_name)
+	var show_alpha := enable_alpha and setting_value.a != 1.0
+	var setting_str: String = setting_value.to_html(show_alpha)
+	color_edit.value = setting_str
+	var default_value: Color = GlobalSettings.default_config[section_name][setting_name]
+	reset_button.visible = (setting_str != default_value.to_html(default_value.a != 1.0))
