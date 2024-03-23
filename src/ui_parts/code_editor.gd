0265f4397964c5afd2e47e0951616ab43a5b91c5
MewPurPur <mew.pur.pur@gmail.com>
2024-03-23 04:39:45 +0200

    Small improvements

--- src/ui_parts/code_editor.gd
+++ src/ui_parts/code_editor.gd
@@ -1,8 +1,6 @@
 extends VBoxContainer
 
 const ContextPopup = preload("res://src/ui_elements/context_popup.tscn")
-const CopyIcon = preload("res://visual/icons/Copy.svg")
-const ClearIcon = preload("res://visual/icons/Clear.svg")
 
 @onready var code_edit: TextEdit = $ScriptEditor/SVGCodeEdit
 @onready var error_bar: PanelContainer = $ScriptEditor/ErrorBar
@@ -129,7 +127,10 @@ func _on_file_button_pressed() -> void:
 
 
 func _on_options_button_pressed():
-	var btn_array: Array[Button] = [Utils.create_btn(tr("Copy All Text"), _on_copy_button_pressed, false, CopyIcon), Utils.create_btn(tr("Reset SVG"), reset_svg, false, ClearIcon)]
+	var btn_array: Array[Button] = [Utils.create_btn(tr("Copy All Text"),
+			_on_copy_button_pressed, false, load("res://visual/icons/Copy.svg")),
+			Utils.create_btn(tr("Reset SVG"), reset_svg, is_svg_default(),
+			load("res://visual/icons/Clear.svg"))]
 	var context_popup := ContextPopup.instantiate()
 	add_child(context_popup)
 	context_popup.set_button_array(btn_array, false, options_button.size.x)
@@ -139,9 +140,13 @@ func _on_options_button_pressed():
 func clear_file_path() -> void:
 	GlobalSettings.modify_save_data("current_file_path", "")
 
+func is_svg_default() -> bool:
+	return SVG.text == '<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"></svg>'
+
 func reset_svg() -> void:
-	SVG.text = '<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"></svg>'
-	SVG.update_tags()
+	if not is_svg_default():
+		SVG.text = '<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"></svg>'
+		SVG.update_tags()
 
 func _notification(what: int) -> void:
 	if what == Utils.CustomNotification.HIGHLIGHT_COLORS_CHANGED:
