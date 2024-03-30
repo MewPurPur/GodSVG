cc6aaf41cc9238c2bb584136ffdcc9f03c0a76e0
RedFurryDemon <55105171+RedFurryDemon@users.noreply.github.com>
2024-03-30 19:15:38 +0100

    Rudimentary <stop/> tag implementation (#627)

---
+++ src/data_classes/TagStop.gd
@@ -0,0 +1,16 @@
+## A <stop/> tag.
+class_name TagStop extends Tag
+
+const name = "stop"
+const possible_conversions = []
+const known_shape_attributes = []
+const known_inheritable_attributes = ["offset", "stop-color", "stop-opacity"]
+const icon = preload("res://visual/icons/tag/stop.svg")
+
+func _init() -> void:
+	attributes = {
+		"offset": AttributeNumeric.new(AttributeNumeric.Mode.NFLOAT, "0"),
+		"stop-color": AttributeColor.new("black"),
+		"stop-opacity": AttributeNumeric.new(AttributeNumeric.Mode.NFLOAT, "1"),
+	}
+	super()
