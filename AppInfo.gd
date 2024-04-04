25952447bec970145ee79bad8f92a29d36f7cc3f
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2024-04-04 19:22:33 +0300

    Take translation info from the POT files (#638)

--- AppInfo.gd
+++ AppInfo.gd
@@ -10,6 +10,7 @@ const project_founder_and_manager: Array[String] = ["MewPurPur"]
 # Entries are formatted as follows:
 # Option 1: Real Name (GitHub username)
 # Option 2: GitHub username
+# Translator credentials for each language are taken from their corresponding POT file.
 
 const authors: Array[String] = [
 	"ajreckof",
@@ -24,13 +25,6 @@ const authors: Array[String] = [
 	"thiagola92",
 	"Tom Blackwell (Volts-s)",
 ]
-
-const translations: Dictionary = {
-	"bg": ["MewPurPur (mew.pur.pur@gmail.com)"],
-	"de": ["Kiisu-Master", "Swarkin"],
-	"ru": ["vovkiv (volkovissocool@gmail.com)"],
-	"uk": ["vovkiv (volkovissocool@gmail.com)"],
-}
 
 # GodSVG is a non-profit project developed by voluntary contributors.
 # Everyone who has generously sponsored the project is listed here.
