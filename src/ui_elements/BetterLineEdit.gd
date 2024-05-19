c5a308b1ca80d485be9667e1eaf2bee3605101fd
MewPurPur <mew.pur.pur@gmail.com>
2024-05-19 03:54:57 +0300

    Fix some LineEdit regressions

--- src/ui_elements/BetterLineEdit.gd
+++ src/ui_elements/BetterLineEdit.gd
@@ -21,7 +21,7 @@ func _ready() -> void:
 	focus_entered.connect(_on_base_class_focus_entered)
 	focus_exited.connect(_on_base_class_focus_exited)
 	mouse_exited.connect(_on_base_class_mouse_exited)
-	text_submitted.connect(_on_base_class_text_submitted)
+	text_submitted.connect(release_focus.unbind(1))
 
 func _input(event: InputEvent) -> void:
 	if has_focus():
@@ -35,9 +35,6 @@ func _input(event: InputEvent) -> void:
 		elif first_click:
 			first_click = false
 			select_all()
-		elif event.is_action_pressed("ui_focus_next") or\
-		event.is_action_pressed("ui_focus_prev"):
-			text_submitted.emit(text)
 
 
 var first_click := false
@@ -52,11 +49,8 @@ func _on_base_class_focus_exited() -> void:
 	if Input.is_action_pressed("ui_cancel"):
 		text = text_before_focus
 		text_change_canceled.emit()
-
-func _on_base_class_text_submitted(_submitted_text) -> void:
-	if not Input.is_action_just_pressed("ui_focus_next") and\
-	not Input.is_action_just_pressed("ui_focus_prev"):
-		release_focus()
+	else:
+		text_submitted.emit(text)
 
 
 func _on_base_class_mouse_exited() -> void:
