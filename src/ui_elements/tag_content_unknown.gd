764a23bda063012d874178227eca5efdcbca493d
MewPurPur <mew.pur.pur@gmail.com>
2024-05-31 01:03:57 +0300

    This PR sucks, but it fixes an immediate issue and sets us up for more improvements overtime

--- src/ui_elements/tag_content_unknown.gd
+++ src/ui_elements/tag_content_unknown.gd
@@ -1,42 +1,4 @@
 extends VBoxContainer
 
-const TransformField = preload("res://src/ui_elements/transform_field.tscn")
-const NumberField = preload("res://src/ui_elements/number_field.tscn")
-const NumberSlider = preload("res://src/ui_elements/number_field_with_slider.tscn")
-const ColorField = preload("res://src/ui_elements/color_field.tscn")
-const EnumField = preload("res://src/ui_elements/enum_field.tscn")
-
-@onready var attribute_container: HFlowContainer = $AttributeContainer
-
 var tag: Tag
 var tid: PackedInt32Array
-
-func _ready() -> void:
-	# Continue with supported attributes.
-	for attribute_key in tag.attributes:
-		var attribute: Attribute = tag.attributes[attribute_key]
-		var input_field: Control
-		if attribute is AttributeTransform:
-			input_field = TransformField.instantiate()
-		elif attribute is AttributeNumeric:
-			var min_value: float = DB.attribute_numeric_bounds[attribute.name].x
-			var max_value: float = DB.attribute_numeric_bounds[attribute.name].y
-			if is_inf(max_value):
-				input_field = NumberField.instantiate()
-				if not is_inf(min_value):
-					input_field.allow_lower = false
-					input_field.min_value = min_value
-			else:
-				input_field = NumberSlider.instantiate()
-				input_field.allow_lower = false
-				input_field.allow_higher = false
-				input_field.min_value = min_value
-				input_field.max_value = max_value
-				input_field.slider_step = 0.01
-		elif attribute is AttributeColor:
-			input_field = ColorField.instantiate()
-		elif attribute is AttributeEnum:
-			input_field = EnumField.instantiate()
-		input_field.attribute = attribute
-		input_field.focus_entered.connect(Indications.normal_select.bind(tid))
-		attribute_container.add_child(input_field)
