b2c95156a73b815eff38d8d384e500c9c8733012
MewPurPur <mew.pur.pur@gmail.com>
2024-06-01 15:10:34 +0300

    Fix some bugs and bump version to alpha 4

--- src/ui_elements/GridDrawingControl.gd
+++ src/ui_elements/GridDrawingControl.gd
@@ -2,7 +2,7 @@ class_name PanelGrid extends GridContainer
 
 const font = preload("res://visual/fonts/Font.ttf")
 
-@export var items: Array[String]
+@export var items: PackedStringArray
 @export var stylebox: StyleBox
 
 func setup() -> void:
