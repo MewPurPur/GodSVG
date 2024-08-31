938707fa5dccf9a269a03600c078eacfd83241c1
MewPurPur <mew.pur.pur@gmail.com>
2024-08-31 14:45:32 +0300

    Remove unused MacOS icon

--- src/ui_parts/about_menu.gd
+++ src/ui_parts/about_menu.gd
@@ -9,9 +9,15 @@ const app_info_json = preload("res://app_info.json")
 @onready var project_founder_list: PanelGrid = %ProjectFounder/List
 @onready var authors_list: PanelGrid = %Developers/List
 
+@onready var donors_vbox: VBoxContainer = %Donors
+@onready var golden_donors_vbox: VBoxContainer = %GoldenDonors
+@onready var diamond_donors_vbox: VBoxContainer = %DiamondDonors
 @onready var donors_list: PanelGrid = %Donors/List
 @onready var golden_donors_list: PanelGrid = %GoldenDonors/List
 @onready var diamond_donors_list: PanelGrid = %DiamondDonors/List
+@onready var past_donors_list: PanelGrid = %PastDonors/List
+@onready var past_golden_donors_list: PanelGrid = %PastGoldenDonors/List
+@onready var past_diamond_donors_list: PanelGrid = %PastDiamondDonors/List
 
 func _ready() -> void:
 	var app_info: Dictionary = app_info_json.data
@@ -20,12 +26,39 @@ func _ready() -> void:
 	project_founder_list.setup()
 	authors_list.items = app_info.authors
 	authors_list.setup()
-	donors_list.items = app_info.donors + ["%d anonymous" % app_info.anonymous_donors]
-	donors_list.setup()
-	golden_donors_list.items = app_info.golden_donors
-	golden_donors_list.setup()
-	diamond_donors_list.items = app_info.diamond_donors
-	diamond_donors_list.setup()
+	# Once the past donors lists start filling up, they will never unfill, so no need to
+	# bother with logic, we can just unhide it manually.
+	if app_info.donors.is_empty() and app_info.anonymous_donors == 0:
+		donors_vbox.hide()
+	else:
+		donors_list.items = app_info.donors
+		if app_info.anonymous_donors != 0:
+			donors_list.items.append("%d anonymous" % app_info.anonymous_donors)
+		donors_list.setup()
+	if app_info.golden_donors.is_empty() and app_info.anonymous_golden_donors != 0:
+		golden_donors_vbox.hide()
+	else:
+		golden_donors_list.items = app_info.golden_donors
+		if app_info.anonymous_golden_donors != 0:
+			golden_donors_list.items.append("%d anonymous" % app_info.anonymous_golden_donors)
+		golden_donors_list.setup()
+	if app_info.diamond_donors.is_empty() and app_info.anonymous_diamond_donors == 0:
+		diamond_donors_vbox.hide()
+	else:
+		diamond_donors_list.items = app_info.diamond_donors
+		if app_info.anonymous_diamond_donors != 0:
+			diamond_donors_list.items.append("%d anonymous" % app_info.anonymous_diamond_donors)
+		diamond_donors_list.setup()
+	
+	past_donors_list.items = app_info.past_donors +\
+			["%d anonymous" % app_info.past_anonymous_donors]
+	past_donors_list.setup()
+	past_golden_donors_list.items = app_info.past_golden_donors +\
+			["%d anonymous" % app_info.past_anonymous_golden_donors]
+	past_golden_donors_list.setup()
+	past_diamond_donors_list.items = app_info.past_diamond_donors +\
+			["%d anonymous" % app_info.past_anonymous_diamond_donors]
+	past_diamond_donors_list.setup()
 	# There can be multiple translators for a single locale.
 	for lang in TranslationServer.get_loaded_locales():
 		var credits := TranslationServer.get_translation_object(lang).get_message(
