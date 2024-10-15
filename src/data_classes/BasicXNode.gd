f010eccb3de97a9bd2a99ed267708c0b931d1cba
MewPurPur <mew.pur.pur@gmail.com>
2024-10-16 00:10:14 +0300

    Unsuccessful attempt to fix issues around XML text nodes

--- src/data_classes/BasicXNode.gd
+++ src/data_classes/BasicXNode.gd
@@ -23,6 +23,46 @@ func _init(new_type: NodeType, new_text: String) -> void:
 func duplicate() -> BasicXNode:
 	return BasicXNode.new(_type, _text)
 
+func can_merge() -> bool:
+	if parent.get_child_count() > xid[-1] + 1:
+		var next_xnode := parent.get_child(xid[-1] + 1)
+		if next_xnode is BasicXNode and next_xnode.get_type() == _type:
+			return true
+	if xid[-1] > 0:
+		var prev_xnode := parent.get_child(xid[-1] - 1)
+		if prev_xnode is BasicXNode and prev_xnode.get_type() == _type:
+			return true
+	return false
+
+func merge() -> void:
+	var merged_text := _text
+	var start_index := xid[-1]
+	var end_index := xid[-1]
+	
+	# Merge with previous and next XML nodes.
+	while start_index > 0:
+		var prev_xnode := parent.get_child(start_index - 1)
+		if prev_xnode is BasicXNode and prev_xnode.get_type() == _type:
+			merged_text = prev_xnode.get_text() + merged_text
+			start_index -= 1
+		else:
+			break
+	
+	while end_index < parent.get_child_count() - 1:
+		var next_xnode := parent.get_child(end_index + 1)
+		if next_xnode is BasicXNode and next_xnode.get_type() == _type:
+			merged_text += next_xnode.get_text()
+			end_index += 1
+		else:
+			break
+	
+	# Remove merged XML nodes.
+	for i in range(end_index, start_index, -1):
+		if i != xid[-1]:
+			parent.remove_child(i)
+	
+	set_text(merged_text)
+
 static func get_type_string(node_type: NodeType) -> String:
 	match node_type:
 		NodeType.COMMENT: return TranslationServer.translate("Comment")
