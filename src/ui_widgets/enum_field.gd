f089c05d466a0c2c709aad35b0e3289aed81e1ee
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2025-02-16 01:10:48 +0200

    Fix merging the last PR early (#1157)

--- src/ui_widgets/enum_field.gd
+++ src/ui_widgets/enum_field.gd
@@ -7,13 +7,13 @@ var attribute_name: String  # May propagate.
 const reload_icon = preload("res://assets/icons/Reload.svg")
 
 func set_value(new_value: String, save := false) -> void:
-	sync(new_value)
 	element.set_attribute(attribute_name, new_value)
+	sync_to_attribute()
 	if save:
 		State.queue_svg_save()
 
 func sync_to_attribute() -> void:
-	set_value(element.get_attribute_value(attribute_name))
+	sync(element.get_attribute_value(attribute_name))
 
 func setup_placeholder() -> void:
 	placeholder_text = element.get_default(attribute_name)
