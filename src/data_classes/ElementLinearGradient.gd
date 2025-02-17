7db89192387bc7ea4f84fb0c1bf56532e1c8d6f5
MewPurPur <mew.pur.pur@gmail.com>
2025-02-17 13:50:12 +0200

    Fix visual issue with currentColor keyword inside gradients

--- src/data_classes/ElementLinearGradient.gd
+++ src/data_classes/ElementLinearGradient.gd
@@ -13,7 +13,7 @@ func _get_own_default(attribute_name: String) -> String:
 		_: return ""
 
 func get_percentage_handling(attribute_name: String) -> DB.PercentageHandling:
-	if get_attribute_value("gradientUnits") == "objectBoundingBox" and\
+	if get_attribute_value("gradientUnits") in ["objectBoundingBox", ""] and\
 	attribute_name in ["x1", "y1", "x2", "y2"]:
 		return DB.PercentageHandling.FRACTION
 	else:
