18b73b116fd17d72ee593c205d869d170fe1b986
MewPurPur <mew.pur.pur@gmail.com>
2025-06-09 20:49:29 +0300

    Update to 4.5.dev5 and fix CLI opening

--- src/autoload/State.gd
+++ src/autoload/State.gd
@@ -4,7 +4,6 @@ extends Node
 const OptionsDialogScene = preload("res://src/ui_widgets/options_dialog.tscn")
 const PathCommandPopupScene = preload("res://src/ui_widgets/path_popup.tscn")
 
-
 signal svg_unknown_change
 signal svg_resized
 
@@ -64,16 +63,9 @@ func _enter_tree() -> void:
 	Configs.active_tab_changed.connect(setup_from_tab)
 	setup_from_tab.call_deferred()  # Let everything load before emitting signals.
 	
-	var cmdline_args := OS.get_cmdline_args()
-	
-	# The first argument passed is always a path to the scene file when in-editor.
-	if (OS.is_debug_build() and not OS.has_feature("template")) and cmdline_args.size() >= 1:
-		cmdline_args.remove_at(0)
-	
-	if cmdline_args.size() >= 1:
-		# Need to wait a frame so the import warnings panel becomes available.
-		await get_tree().process_frame
-		FileUtils.apply_svgs_from_paths(cmdline_args)
+	# Need to wait a frame so the import warnings panel becomes available.
+	await get_tree().process_frame
+	FileUtils.apply_svgs_from_paths(OS.get_cmdline_args(), false)
 
 func setup_from_tab() -> void:
 	var active_tab := Configs.savedata.get_active_tab()
