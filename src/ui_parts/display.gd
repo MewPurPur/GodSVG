f2a98375c656fea37a3d15f473cda54575b2a4e4
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2025-06-11 23:38:04 +0300

    Update display.gd

--- src/ui_parts/display.gd
+++ src/ui_parts/display.gd
@@ -149,7 +149,7 @@ func _update_input_debug(event: InputEvent) -> void:
 		var new_text := input_debug_label.text
 		var event_text := event.as_text()
 		if event is InputEventMouse:
-			event_text += " " + str(event.position)
+			event_text += " (" + String.num(event.position.x) = ", " + String.num(event.position.y) + ")"
 		if event_text == last_event_text:
 			last_event_repeat_count += 1
 			new_text = new_text.left(new_text.rfind("\n", new_text.length() - 2) + 1)
