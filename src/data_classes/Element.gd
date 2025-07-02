54d398fc5dcbec21bfb821258ce2306704bf79ca
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2025-07-02 20:17:14 +0300

    Fix cropout bug with percentages (#1389)

--- src/data_classes/Element.gd
+++ src/data_classes/Element.gd
@@ -187,9 +187,9 @@ func get_attribute_num(attribute_name: String) -> float:
 				DB.PercentageHandling.NORMALIZED: return 1024 * num
 		else:
 			match percentage_handling:
-				DB.PercentageHandling.HORIZONTAL: return svg.width * num
-				DB.PercentageHandling.VERTICAL: return svg.height * num
-				DB.PercentageHandling.NORMALIZED: return svg.normalized_diagonal * num
+				DB.PercentageHandling.HORIZONTAL: return svg.viewbox.size.x * num
+				DB.PercentageHandling.VERTICAL: return svg.viewbox.size.y * num
+				DB.PercentageHandling.NORMALIZED: return svg.viewbox_normalized_diagonal * num
 	return num
 
 func is_attribute_percentage(attribute_name: String) -> bool:
