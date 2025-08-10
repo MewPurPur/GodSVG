254e64ff80f0e949c907ed32a70bf6d906cca57b
MewPurPur <mew.pur.pur@gmail.com>
2025-08-11 00:07:05 +0300

    Remove dependencies to the zoom widget in viewport

--- src/ui_parts/viewport.gd
+++ src/ui_parts/viewport.gd
@@ -1,66 +1,54 @@
 extends SubViewport
 
-const ZoomMenu = preload("res://src/ui_widgets/zoom_menu.gd")
-const HandlesManager = preload("res://src/ui_parts/handles_manager.gd")
-const DisplayTexture = preload("res://src/ui_parts/display_texture.gd")
-
 const BUFFER_VIEW_SPACE = 0.8
 const ZOOM_RESET_BUFFER = 0.875
 
 # Holds zoom position for Ctrl + MMB zooming.
 var _zoom_to: Vector2
 
-var limit_left := 0.0
-var limit_right := 0.0
-var limit_top := 0.0
-var limit_bottom := 0.0
+var limit_top_left := Vector2.ZERO
+var limit_bottom_right := Vector2.ZERO
 
-@onready var checkerboard: TextureRect = $Checkerboard
 @onready var view: SubViewportContainer = get_parent()
-@onready var controls: HandlesManager = $Controls
-@onready var display_texture: DisplayTexture = $Checkerboard/DisplayTexture
-@onready var zoom_menu: ZoomMenu = %ZoomMenu
-
 
 func _ready() -> void:
-	zoom_menu.zoom_changed.connect(_on_zoom_changed)
-	zoom_menu.zoom_reset_pressed.connect(center_frame)
-	zoom_menu.zoom_changed.connect(view.update.unbind(2))
 	size_changed.connect(adjust_view)
-	State.svg_resized.connect(resize)
-	resize()
-	Configs.active_tab_changed.connect(zoom_menu.zoom_reset)
+	State.svg_resized.connect(center_frame)
+	center_frame()
+	Configs.active_tab_changed.connect(center_frame)
 	await get_tree().process_frame
-	zoom_menu.zoom_reset()
+	center_frame()
+
+
+func set_zoom(new_zoom: float, offset := Vector2(0.5, 0.5)) -> void:
+	new_zoom = clampf(new_zoom, Canvas.MIN_ZOOM, Canvas.MAX_ZOOM)
+	State.set_zoom(new_zoom)
+	adjust_view(offset)
+	view.update()
 
 # Top left corner.
 func set_view(new_position: Vector2) -> void:
 	var scaled_size := size / State.zoom
-	view.camera_position = new_position.clamp(Vector2(limit_left, limit_top), Vector2(limit_right, limit_bottom) - scaled_size)
+	view.camera_position = new_position.clamp(limit_top_left, limit_bottom_right - scaled_size)
 	
 	var stripped_left := maxf(view.camera_position.x, 0.0)
 	var stripped_top := maxf(view.camera_position.y, 0.0)
 	var stripped_right := minf(view.camera_position.x + scaled_size.x, State.root_element.width)
 	var stripped_bottom := minf(view.camera_position.y + scaled_size.y, State.root_element.height)
-	display_texture.view_rect = Rect2(stripped_left, stripped_top, stripped_right - stripped_left, stripped_bottom - stripped_top)
+	view.view_rect = Rect2(stripped_left, stripped_top, stripped_right - stripped_left, stripped_bottom - stripped_top)
 	view.update()
 
-
-# Adjust the SVG dimensions.
-func resize() -> void:
-	var root_element_size := State.root_element.get_size()
-	if root_element_size.is_finite():
-		checkerboard.size = root_element_size
-	zoom_menu.zoom_reset()
 
 func center_frame() -> void:
 	var available_size := size * ZOOM_RESET_BUFFER
 	var w_ratio := available_size.x / State.root_element.width
 	var h_ratio := available_size.y / State.root_element.height
 	if is_finite(w_ratio) and is_finite(h_ratio):
-		zoom_menu.set_zoom(nearest_po2(ceili(minf(w_ratio, h_ratio) * 32)) / 64.0)
+		var new_zoom := nearest_po2(ceili(minf(w_ratio, h_ratio) * 32)) / 64.0
+		State.set_zoom(new_zoom)
 	else:
-		zoom_menu.set_zoom(1.0)
+		State.set_zoom(1.0)
+	
 	adjust_view()
 	set_view((State.root_element.get_size() - size / State.zoom) / 2)
 
@@ -74,46 +62,46 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.ctrl_pressed and event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
 			if _zoom_to == Vector2.ZERO:  # Set zoom position if starting action.
 				_zoom_to = get_mouse_position() / Vector2(size)
-			zoom_menu.set_zoom(State.zoom * (1.0 + (1 if Configs.savedata.invert_zoom else -1) *\
-					(wrap_mouse(event.relative).y if Configs.savedata.wraparound_panning else event.relative.y) / 128.0), _zoom_to)
-		# Panning with LMB or MMB. This gives a reliable way to adjust the view without dragging the things on it.
+			var relative_y: float = wrap_mouse(event.relative).y if Configs.savedata.wraparound_panning else event.relative.y
+			var new_zoom := State.zoom * (1.0 + (1 if Configs.savedata.invert_zoom else -1) * relative_y / 128.0)
+			set_zoom(new_zoom, _zoom_to)
 		else:
-			set_view(view.camera_position - (wrap_mouse(event.relative) if Configs.savedata.wraparound_panning else event.relative) / State.zoom)
+			# Panning with LMB or MMB. This gives a reliable way to adjust the view without dragging the things on it.
+			var relative: Vector2 = wrap_mouse(event.relative) if Configs.savedata.wraparound_panning else event.relative
+			set_view(view.camera_position - relative / State.zoom)
 	
-	elif event is InputEventPanGesture and not DisplayServer.get_name() == "Android":
-		# Zooming with Ctrl + touch?
+	elif event is InputEventPanGesture and DisplayServer.get_name() != "Android":
 		if event.ctrl_pressed:
-			zoom_menu.set_zoom(State.zoom * (1 + event.delta.y / 2))
-		# Panning with touch.
+			# Zooming with Ctrl + touch?
+			set_zoom(State.zoom * (1.0 + event.delta.y / 2.0))
 		else:
-			set_view(view.camera_position + event.delta * 32 / State.zoom)
-	# Zooming with touch.
+			# Panning with touch.
+			set_view(view.camera_position + event.delta * 32.0 / State.zoom)
 	elif event is InputEventMagnifyGesture:
-		zoom_menu.set_zoom(State.zoom * event.factor)
-	# Actions with scrolling.
+		# Zooming with touch.
+		set_zoom(State.zoom * event.factor)
 	elif event is InputEventMouseButton and event.is_pressed():
+		# Actions with scrolling.
 		var move_vec := Vector2.ZERO
 		var zoom_dir := 0
 		
-		# Zooming with scrolling.
 		if (not event.ctrl_pressed and not event.shift_pressed and not Configs.savedata.use_ctrl_for_zoom) or\
 		(event.ctrl_pressed and Configs.savedata.use_ctrl_for_zoom):
+			# Zooming with scrolling.
 			match event.button_index:
-				MOUSE_BUTTON_WHEEL_UP when Configs.savedata.invert_zoom: zoom_dir = -1
-				MOUSE_BUTTON_WHEEL_DOWN when Configs.savedata.invert_zoom: zoom_dir = 1
-				MOUSE_BUTTON_WHEEL_UP: zoom_dir = 1
-				MOUSE_BUTTON_WHEEL_DOWN: zoom_dir = -1
+				MOUSE_BUTTON_WHEEL_UP: zoom_dir = -1 if Configs.savedata.invert_zoom else 1
+				MOUSE_BUTTON_WHEEL_DOWN: zoom_dir = 1 if Configs.savedata.invert_zoom else -1
 				_: return
-		# Inverted panning with Shift + scrolling.
 		elif event.shift_pressed:
+			# Inverted panning with Shift + scrolling.
 			match event.button_index:
 				MOUSE_BUTTON_WHEEL_UP: move_vec = Vector2.LEFT
 				MOUSE_BUTTON_WHEEL_DOWN: move_vec = Vector2.RIGHT
 				MOUSE_BUTTON_WHEEL_LEFT: move_vec = Vector2.UP
 				MOUSE_BUTTON_WHEEL_RIGHT: move_vec = Vector2.DOWN
 				_: return
-		# Panning with scrolling.
 		else:
+			# Panning with scrolling.
 			match event.button_index:
 				MOUSE_BUTTON_WHEEL_UP: move_vec = Vector2.UP
 				MOUSE_BUTTON_WHEEL_DOWN: move_vec = Vector2.DOWN
@@ -126,10 +114,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		var factor: float = event.factor
 		if factor == roundf(factor):  # Detects if precise factor is unsuported.
 			factor = 1.0
+		
+		var multiplied_factor := sqrt(2) * factor
 		if zoom_dir == 1:
-			zoom_menu.zoom_in(factor, mouse_offset)
+			set_zoom(State.zoom * multiplied_factor, mouse_offset)
 		elif zoom_dir == -1:
-			zoom_menu.zoom_out(factor, mouse_offset)
+			set_zoom(State.zoom / multiplied_factor, mouse_offset)
 		
 		set_view(view.camera_position + move_vec * factor / State.zoom * 32)
 	
@@ -140,25 +130,16 @@ func _unhandled_input(event: InputEvent) -> void:
 				_zoom_to = Vector2.ZERO  # Reset Ctrl + MMB zoom position if released.
 
 
-func _on_zoom_changed(new_zoom_level: float, offset: Vector2) -> void:
-	State.set_zoom(new_zoom_level)
-	adjust_view(offset)
-	checkerboard.material.set_shader_parameter("uv_scale", nearest_po2(int(State.zoom * 32.0)) / 32.0)
-
 var last_size_adjusted := size / State.zoom
 
 func adjust_view(offset := Vector2(0.5, 0.5)) -> void:
 	var old_size := last_size_adjusted
 	last_size_adjusted = size / State.zoom
 	
-	var svg_w := State.root_element.width if State.root_element.has_attribute("width") else 16384.0
-	var svg_h := State.root_element.height if State.root_element.has_attribute("height") else 16384.0
-	
 	var zoomed_size := BUFFER_VIEW_SPACE * size / State.zoom
-	limit_left = -zoomed_size.x
-	limit_right = zoomed_size.x + svg_w
-	limit_top = -zoomed_size.y
-	limit_bottom = zoomed_size.y + svg_h
+	limit_top_left = -zoomed_size
+	limit_bottom_right = zoomed_size + Vector2(State.root_element.width if State.root_element.has_attribute("width") else 16384.0,
+			State.root_element.height if State.root_element.has_attribute("height") else 16384.0)
 	
 	set_view(Vector2(lerpf(view.camera_position.x, view.camera_position.x + old_size.x - size.x / State.zoom, offset.x),
 			lerpf(view.camera_position.y, view.camera_position.y + old_size.y - size.y / State.zoom, offset.y)))
--- src/ui_parts/viewport_container.gd
+++ src/ui_parts/viewport_container.gd
@@ -1,4 +1,7 @@
-extends SubViewportContainer
+extends Canvas
+
+const ViewportControls = preload("res://src/ui_parts/viewport.gd")
+const HandlesManager = preload("res://src/ui_parts/handles_manager.gd")
 
 const TICKS_INTERVAL = 4
 const TICK_DISTANCE = float(TICKS_INTERVAL)
@@ -11,12 +14,40 @@ var camera_zoom: float
 var camera_position: Vector2
 var camera_snapped_position: Vector2
 
-@onready var viewport: SubViewport = $Viewport
-@onready var checkerboard: TextureRect = $Viewport/Checkerboard
-
+var viewport := ViewportControls.new()
+var display_texture := TextureRect.new()
+var handles_manager := HandlesManager.new()
+var checkerboard := TextureRect.new()
 var reference_texture_rect: TextureRect
 
+func _enter_tree() -> void:
+	viewport.size_2d_override_stretch = true
+	viewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+	viewport.disable_3d = true
+	viewport.handle_input_locally = false
+	viewport.gui_snap_controls_to_pixels = false
+	add_child(viewport)
+	checkerboard.texture = load("res://assets/icons/Checkerboard.svg")
+	checkerboard.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	checkerboard.stretch_mode = TextureRect.STRETCH_TILE
+	checkerboard.texture_filter = TEXTURE_FILTER_NEAREST
+	var zoom_shader_material := ShaderMaterial.new()
+	zoom_shader_material.shader = load("res://src/shaders/zoom_shader.gdshader")
+	checkerboard.material = zoom_shader_material
+	viewport.add_child(checkerboard)
+	display_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	display_texture.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
+	checkerboard.add_child(display_texture)
+	handles_manager.mouse_filter = Control.MOUSE_FILTER_PASS
+	viewport.add_child(handles_manager)
+
 func _ready() -> void:
+	State.svg_changed.connect(queue_texture_update)
+	State.view_rasterized_changed.connect(_on_view_rasterized_changed)
+	queue_texture_update()
+	State.zoom_changed.connect(_on_zoom_changed)
+	State.svg_resized.connect(_on_svg_resized)
+	
 	Configs.active_tab_changed.connect(sync_reference_image)
 	Configs.active_tab_reference_changed.connect(sync_reference_image)
 	Configs.grid_color_changed.connect(queue_redraw)
@@ -25,20 +56,35 @@ func _ready() -> void:
 	RenderingServer.canvas_item_set_parent(grid_ci, ci)
 	RenderingServer.canvas_item_set_parent(grid_numbers_ci, ci)
 	State.svg_resized.connect(queue_redraw)
-	State.zoom_changed.connect(change_zoom)
-	State.zoom_changed.connect(queue_redraw)
 
 func exit_tree() -> void:
 	RenderingServer.free_rid(grid_ci)
 	RenderingServer.free_rid(grid_numbers_ci)
 
-func change_zoom() -> void:
+func _on_svg_resized() -> void:
+	var root_element_size := State.root_element.get_size()
+	if root_element_size.is_finite():
+		checkerboard.size = root_element_size
+
+func _on_zoom_changed() -> void:
+	checkerboard.material.set_shader_parameter("uv_scale", nearest_po2(int(State.zoom * 32.0)) / 32.0)
 	camera_zoom = State.zoom
+	queue_texture_update()
+	queue_redraw()
 
 func update_show_grid() -> void:
 	RenderingServer.canvas_item_set_visible(grid_ci, State.show_grid)
 	RenderingServer.canvas_item_set_visible(grid_numbers_ci, State.show_grid)
 
+
+func center_frame() -> void:
+	viewport.center_frame()
+
+func zoom_in() -> void:
+	viewport.set_zoom(State.zoom * sqrt(2))
+
+func zoom_out() -> void:
+	viewport.set_zoom(State.zoom / sqrt(2))
 
 func sync_reference_image() -> void:
 	var active_tab := Configs.savedata.get_active_tab()
@@ -155,3 +201,46 @@ func _draw() -> void:
 		pca.resize(minor_points.size() / 2)
 		pca.fill(minor_grid_color)
 		RenderingServer.canvas_item_add_multiline(grid_ci, minor_points, pca)
+
+
+var view_rect := Rect2():
+	set(new_value):
+		if view_rect != new_value:
+			view_rect = new_value
+			queue_texture_update()
+
+var _texture_update_pending := false
+
+func _on_view_rasterized_changed() -> void:
+	if State.zoom != 1.0:
+		queue_texture_update()
+
+
+func queue_texture_update() -> void:
+	_texture_update.call_deferred()
+	_texture_update_pending = true
+
+func _texture_update() -> void:
+	if not _texture_update_pending:
+		return
+	
+	_texture_update_pending = false
+	
+	var image_zoom := 1.0 if State.view_rasterized and State.zoom > 1.0 else State.zoom
+	var pixel_size := 1 / image_zoom
+	
+	# Translate to canvas coords.
+	var display_rect := view_rect.grow(pixel_size * 2)
+	display_rect.position = display_rect.position.snapped(Vector2(pixel_size, pixel_size))
+	display_rect.position.x = maxf(display_rect.position.x, 0.0)
+	display_rect.position.y = maxf(display_rect.position.y, 0.0)
+	display_rect.size = display_rect.size.snapped(Vector2(pixel_size, pixel_size))
+	display_rect.end.x = minf(display_rect.end.x, ceili(State.root_element.width))
+	display_rect.end.y = minf(display_rect.end.y, ceili(State.root_element.height))
+	
+	var svg_text := SVGParser.root_cutout_to_markup(State.root_element, display_rect.size.x,
+			display_rect.size.y, Rect2(State.root_element.world_to_canvas(display_rect.position),
+			display_rect.size / State.root_element.canvas_transform.get_scale()))
+	Utils.set_control_position_fixed(display_texture, display_rect.position)
+	display_texture.set_deferred("size", display_rect.size)
+	display_texture.texture = SVGTexture.create_from_string(svg_text, image_zoom)
