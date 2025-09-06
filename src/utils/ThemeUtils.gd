8efda6636db1e1f5dcbf4cbcda7c209b6c6bb326
MewPurPur <mew.pur.pur@gmail.com>
2025-09-06 16:22:56 +0300

    Make folder color darker on light theme

--- src/utils/ThemeUtils.gd
+++ src/utils/ThemeUtils.gd
@@ -16,6 +16,7 @@ static var black_or_white_counter_accent_color: Color
 
 static var warning_icon_color: Color
 static var info_icon_color: Color
+static var folder_color: Color
 
 static var hover_selected_inspector_frame_inner_color: Color
 static var hover_selected_inspector_frame_title_color: Color
@@ -120,6 +121,7 @@ static func recalculate_colors() -> void:
 	
 	warning_icon_color = Color("fca") if is_theme_dark else Color("96592c")
 	info_icon_color = Color("acf") if is_theme_dark else Color("3a6ab0")
+	folder_color = Color("88b6dd") if is_theme_dark else Color("528fcc")
 	
 	hover_selected_inspector_frame_inner_color = Color.from_hsv(0.625, 0.48, 0.27) if ThemeUtils.is_theme_dark else Color.from_hsv(0.625, 0.27, 0.925)
 	hover_selected_inspector_frame_title_color = hover_selected_inspector_frame_inner_color.lerp(max_contrast_color, 0.02)
