9317b782e00607f207d36b998ca26ac2e7c5ca09
MewPurPur <mew.pur.pur@gmail.com>
2026-08-11 15:09:53 +0300

    Integrate Slovenian translation

--- src/ui_widgets/palette_config.gd
+++ src/ui_widgets/palette_config.gd
@@ -163,17 +163,15 @@ func _on_palette_button_pressed() -> void:
 	var palette_idx := find_palette_index()
 	var btn_arr: Array[ContextButton] = []
 	btn_arr.append(ContextButton.create_custom(Translator.translate("Rename"), popup_edit_name, preload("res://assets/icons/Rename.svg")))
+	btn_arr.append(ContextButton.create_custom(Translator.translate("Apply Preset"), open_palette_options, preload("res://assets/icons/Import.svg")))
 	if palette_idx >= 1:
 		btn_arr.append(ContextButton.create_custom(Translator.translate("Move Up"), move_up, preload("res://assets/icons/MoveUp.svg")))
 	if palette_idx < Configs.savedata.get_palette_count() - 1:
 		btn_arr.append(ContextButton.create_custom(Translator.translate("Move Down"), move_down, preload("res://assets/icons/MoveDown.svg")))
-	btn_arr.append(ContextButton.create_custom(Translator.translate("Apply Preset"), open_palette_options, preload("res://assets/icons/Import.svg")))
 	btn_arr.append(ContextButton.create_custom(Translator.translate("Delete"), delete, preload("res://assets/icons/Delete.svg")))
-	
-	var separator_arr := PackedInt32Array([btn_arr.size()])
-	
 	btn_arr.append(ContextButton.create_custom(Translator.translate("Copy as XML"), copy_palette.bind(palette_idx), preload("res://assets/icons/Copy.svg")))
 	btn_arr.append(ContextButton.create_custom(Translator.translate("Save as XML"), save_palette.bind(palette_idx), preload("res://assets/icons/Export.svg")))
 	
+	var separator_arr := PackedInt32Array([2, btn_arr.size() - 2])
 	var context_popup := ContextPopup.create(btn_arr, true, -1, separator_arr)
 	HandlerGUI.popup_under_rect_center(context_popup, palette_button.get_global_rect(), get_viewport())
