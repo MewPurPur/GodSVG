2d204e5d47f2faac9bb3c1f2690bfa0f87da4bab
Mew Pur Pur <85438892+MewPurPur@users.noreply.github.com>
2026-09-21 11:03:19 +0300

    Add ellipse warnings (#1845)

--- src/data_classes/ElementEllipse.gd
+++ src/data_classes/ElementEllipse.gd
@@ -28,8 +28,8 @@ func get_replacement(new_element: String) -> Element:
 			dropped_attributes = PackedStringArray(["rx", "ry", "r"])
 			element.set_attribute("r", get_attribute_value("rx"))
 		"rect":
-			var rx := get_rx()
-			var ry := get_ry()
+			var rx := get_attribute_num("rx")
+			var ry := get_attribute_num("ry")
 			dropped_attributes = PackedStringArray(["cx", "cy", "x", "y", "width", "height"])
 			element.set_attribute("x", get_attribute_num("cx") - rx)
 			element.set_attribute("y", get_attribute_num("cy") - ry)
@@ -39,8 +39,8 @@ func get_replacement(new_element: String) -> Element:
 			dropped_attributes = PackedStringArray(["cx", "cy", "rx", "ry", "d"])
 			var cx := get_attribute_num("cx")
 			var cy := get_attribute_num("cy")
-			var rx := get_rx()
-			var ry := get_ry()
+			var rx := get_attribute_num("rx")
+			var ry := get_attribute_num("ry")
 			var commands: Array[PathCommand] = []
 			commands.append(PathCommand.MoveCommand.new(cx + rx, cy, true))
 			commands.append(PathCommand.EllipticalArcCommand.new(rx, ry, 0, 0, 1, cx - rx, cy, true))
@@ -51,20 +51,28 @@ func get_replacement(new_element: String) -> Element:
 	return element
 
 
-func get_rx() -> float:
-	return get_attribute_num("rx") if has_attribute("rx") else get_attribute_num("ry") if has_attribute("ry") else 0.0
-
-func get_ry() -> float:
-	return get_attribute_num("ry") if has_attribute("ry") else get_attribute_num("rx") if has_attribute("rx") else 0.0
-
-
 func _get_own_default(attribute_name: String) -> String:
 	match attribute_name:
 		"cx", "cy": return "0"
-		"rx": return "auto"
-		"ry": return "auto"
 		"opacity": return "1"
 		_: return ""
 
 func get_bounding_box() -> Rect2:
-	return Rect2(get_attribute_num("cx") - get_rx(), get_attribute_num("cy") - get_ry(), get_rx() * 2, get_ry() * 2)
+	var rx := get_attribute_num("rx")
+	var ry := get_attribute_num("ry")
+	return Rect2(get_attribute_num("cx") - rx, get_attribute_num("cy") - ry, rx * 2, ry * 2)
+
+func get_config_warnings() -> PackedStringArray:
+	var warnings := super()
+	
+	for r_attrib in ["rx", "ry"]:
+		if not has_attribute(r_attrib):
+			var warning := Translator.translate("No \"{attribute_name}\" attribute defined.").format({"attribute_name": r_attrib})
+			warning += " " + Translator.translate("This may be interpreted differently by different SVG software.")
+			warnings.append(warning)
+		else:
+			var r_attrib_value := get_attribute_num(r_attrib)
+			if r_attrib_value <= 0:
+				warnings.append(Translator.translate("Invalid value for \"{attribute_name}\" attribute.").format({"attribute_name": r_attrib}))
+	
+	return warnings
